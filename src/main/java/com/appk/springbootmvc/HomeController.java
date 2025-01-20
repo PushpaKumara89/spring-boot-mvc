8b937beab9b00e3a68beffb62d857918b8157e1c
PushpaKumara89 <kpushpakumarajd@gmail.com>
2025-01-20 23:29:57 +0530

    ModelAndView

--- src/main/java/com/appk/springbootmvc/HomeController.java
+++ src/main/java/com/appk/springbootmvc/HomeController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -13,10 +14,13 @@ public class HomeController {
     }
 
     @RequestMapping("/add")
-    public String add(@RequestParam("num1") int i, @RequestParam("num2")int j, HttpSession session) {
-        int sum = i + j;
+    public ModelAndView add(@RequestParam("num1") int i, @RequestParam("num2")int j) {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("result.jsp");
 
-        session.setAttribute("sum", sum);
-        return "result.jsp";
+        int sum = i + j;
+        mv.addObject("sum", sum);
+
+        return mv;
     }
 }
