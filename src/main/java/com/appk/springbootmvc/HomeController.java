1ba9f248c5f6eaa231b4ac9f3885345150526c05
PushpaKumara89 <kpushpakumarajd@gmail.com>
2025-01-20 23:47:25 +0530

    Prefix and Suffix

--- src/main/java/com/appk/springbootmvc/HomeController.java
+++ src/main/java/com/appk/springbootmvc/HomeController.java
@@ -10,13 +10,13 @@ import org.springframework.web.servlet.ModelAndView;
 public class HomeController {
     @RequestMapping("/")
     public String home(){
-        return "index.jsp";
+        return "index";
     }
 
     @RequestMapping("/add")
     public ModelAndView add(@RequestParam("num1") int i, @RequestParam("num2")int j) {
         ModelAndView mv = new ModelAndView();
-        mv.setViewName("result.jsp");
+        mv.setViewName("result");
 
         int sum = i + j;
         mv.addObject("sum", sum);
