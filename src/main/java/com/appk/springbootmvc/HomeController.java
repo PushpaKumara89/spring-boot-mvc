9fbb0eb88d543ca87e2954a078638bfa383f6f3e
PushpaKumara89 <kpushpakumarajd@gmail.com>
2025-01-20 23:58:16 +0530

    Model and ModelMap

--- src/main/java/com/appk/springbootmvc/HomeController.java
+++ src/main/java/com/appk/springbootmvc/HomeController.java
@@ -1,7 +1,7 @@
 package com.appk.springbootmvc;
 
-import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -14,13 +14,10 @@ public class HomeController {
     }
 
     @RequestMapping("/add")
-    public ModelAndView add(@RequestParam("num1") int i, @RequestParam("num2")int j) {
-        ModelAndView mv = new ModelAndView();
-        mv.setViewName("result");
-
+    public String add(@RequestParam("num1") int i, @RequestParam("num2")int j, ModelMap mm) { // Model m
         int sum = i + j;
-        mv.addObject("sum", sum);
+        mm.addAttribute("sum", sum);
 
-        return mv;
+        return "result";
     }
 }
