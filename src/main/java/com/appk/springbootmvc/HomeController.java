5322ba00b1522172c63e88f8ce336c649289196b
PushpaKumara89 <kpushpakumarajd@gmail.com>
2025-01-23 22:58:30 +0530

    modelAttribute

--- src/main/java/com/appk/springbootmvc/HomeController.java
+++ src/main/java/com/appk/springbootmvc/HomeController.java
@@ -4,6 +4,7 @@ import com.appk.springbootmvc.model.Alien;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -22,14 +23,14 @@ public class HomeController {
         return "result";
     }
 
-    @RequestMapping("/addAlien")
-    public String addAlien(@RequestParam("aid") int aid, @RequestParam("aname") String aname, Model m) {
-
-        Alien a = new Alien();
-        a.setAid(aid);
-        a.setAname(aname);
-
+    /*@RequestMapping("/addAlien")
+    public String addAlien(@ModelAttribute Alien a, Model m) {
         m.addAttribute("alien", a);
+        return "result";
+    }*/
+
+    @RequestMapping("/addAlien")
+    public String addAlien(@ModelAttribute("a1") Alien a) {
         return "result";
     }
 }
